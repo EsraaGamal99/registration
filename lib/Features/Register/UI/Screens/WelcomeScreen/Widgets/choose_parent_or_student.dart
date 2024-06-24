707e9369c1cd3b70d0551ad7c87ba83311660e5a
earaagamal99 <esraa66m@gmail.com>
2024-06-24 16:50:08 +0300

    make sudent and parent selectable

--- lib/Features/Register/UI/Screens/WelcomeScreen/Widgets/choose_parent_or_student.dart
+++ lib/Features/Register/UI/Screens/WelcomeScreen/Widgets/choose_parent_or_student.dart
@@ -1,11 +1,22 @@
-import 'package:elmadsarah/Features/Register/UI/Screens/WelcomeScreen/Widgets/parent_or_student.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:elmadsarah/Features/Register/UI/Screens/WelcomeScreen/Widgets/parent_or_student.dart';
 
-class ChooseParentOrStudent extends StatelessWidget {
-  const ChooseParentOrStudent({
-    super.key,
-  });
+class ChooseParentOrStudent extends StatefulWidget {
+  const ChooseParentOrStudent({super.key});
+
+  @override
+  _ChooseParentOrStudentState createState() => _ChooseParentOrStudentState();
+}
+
+class _ChooseParentOrStudentState extends State<ChooseParentOrStudent> {
+  String selectedType = '';
+
+  void selectType(String type) {
+    setState(() {
+      selectedType = type;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +24,20 @@ class ChooseParentOrStudent extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const ParentOrStudent(
+          ParentOrStudent(
             type: 'طالب',
             backgroundColor: Colors.grey,
+            isSelected: selectedType == 'طالب',
+            onSelect: selectType,
           ),
           SizedBox(
             width: 40.w,
           ),
-          const ParentOrStudent(
+          ParentOrStudent(
             type: 'ولي أمر',
             backgroundColor: Colors.blue,
+            isSelected: selectedType == 'ولي أمر',
+            onSelect: selectType,
           ),
         ],
       ),
--- lib/Features/Register/UI/Screens/WelcomeScreen/Widgets/parent_or_student.dart
+++ lib/Features/Register/UI/Screens/WelcomeScreen/Widgets/parent_or_student.dart
@@ -1,63 +1,55 @@
-import 'package:elmadsarah/Core/Theming/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:elmadsarah/Core/Theming/colors.dart';
 
-class ParentOrStudent extends StatefulWidget {
+class ParentOrStudent extends StatelessWidget {
   final String type;
   final Color backgroundColor;
+  final bool isSelected;
+  final Function(String) onSelect;
+
   const ParentOrStudent({
-    super.key, required this.type, required this.backgroundColor,
+    super.key,
+    required this.type,
+    required this.backgroundColor,
+    required this.isSelected,
+    required this.onSelect,
   });
 
   @override
-  State<ParentOrStudent> createState() => _ParentOrStudentState();
-}
-
-class _ParentOrStudentState extends State<ParentOrStudent> {
-  bool selected = false;
-  int index = 0;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    selected = false;
-    index = 0;
-  }
-  @override
   Widget build(BuildContext context) {
-
     return InkWell(
       onTap: () {
-        selected = !selected;
-        index++;
-        setState(() {
-
-          debugPrint(selected.toString());
-          debugPrint(index.toString());
-
-        });
-
+        onSelect(type);
       },
       child: Column(
         children: [
           CircleAvatar(
-            backgroundColor: widget.backgroundColor,
+            backgroundColor: backgroundColor,
             radius: 50.r,
           ),
           SizedBox(height: 10.h,),
           Row(
             children: [
-              selected ? Container(
-                width: 25,
-                height: 25,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.r),
-                  border: Border.all(color: kPrimaryColor,width: 3),
+              if (isSelected)
+                Container(
+                  width: 25,
+                  height: 25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.r),
+                    border: Border.all(color: kPrimaryColor, width: 3),
+                  ),
+                  child: Icon(Icons.check, color: kPrimaryColor, size: 18.sp,),
                 ),
-                child: Icon(Icons.check,color: kPrimaryColor,size: 18.sp,),
-              ):Container(),
               SizedBox(width: 8.w,),
-              Text(widget.type,style: TextStyle(fontSize: 16.sp,fontWeight: FontWeight.w700,color: selected?kPrimaryColor:Colors.grey),),
+              Text(
+                type,
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w700,
+                  color: isSelected ? kPrimaryColor : Colors.grey,
+                ),
+              ),
             ],
           ),
         ],
@@ -65,4 +57,3 @@ class _ParentOrStudentState extends State<ParentOrStudent> {
     );
   }
 }
-
