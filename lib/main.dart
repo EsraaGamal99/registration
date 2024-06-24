6dbd4cf7eec35317e3d5559d8c8e5854da87dc6c
earaagamal99 <esraa66m@gmail.com>
2024-06-24 04:22:41 +0300

    add flutter localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'Core/Routing/app_router.dart';
