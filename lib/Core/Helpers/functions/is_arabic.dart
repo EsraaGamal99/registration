9cf46c2c6fe1593c0a602bdc12d975e110f45387
earaagamal99 <esraa66m@gmail.com>
2024-06-24 17:32:02 +0300

    navigation handle

--- lib/Core/Helpers/functions/is_arabic.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-bool isArabic(BuildContext context) {
-  // Get the locale of the app
-  Locale locale = Localizations.localeOf(context);
-  
-  // Check if the language code is Arabic
-  return locale.languageCode == 'ar';
-}
---
+++ lib/Core/Routing/routs_name.dart
@@ -0,0 +1,14 @@
+abstract class RoutsName {
+  static const  welcomeScreen             = '/';
+  static const formScreen                 = '/formScreen';
+  static const  specifyScreen             = '/specifyScreen';
+  static const  materialScreen            = '/materialScreen';
+  static const  courseScreen              = '/courseScreen';
+  static const  purposeScreen             = '/purposeScreen';
+  static const  countScreen               = '/countScreen';
+  static const  dayScreen                 = '/dayScreen';
+  static const  periodScreen              = '/periodScreen';
+  static const  subscriptionScreen        = '/subscriptionScreen';
+  static const  payScreen                 = '/payScreen';
+  static const  classScreen               = '/classScreen';
+}
